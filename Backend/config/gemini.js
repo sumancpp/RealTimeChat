e648ad8d63290c6a768b25f77008e953389777fc
Suman Maity <someone.cpp@proton.me>
2026-06-07 14:26:44 +0530

    update gamini

--- Backend/config/gemini.js
+++ Backend/config/gemini.js
@@ -1,7 +1,10 @@
 import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({
-    apiKey: process.env.GEMINI_API_KEY
+
+    apiKey:
+        process.env.GEMINI_API_KEY
+
 });
 
 export default ai;
