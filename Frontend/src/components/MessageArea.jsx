9c910a2e67439ea50449642b3bcdfb1170e964c7
Suman Maity <someone.cpp@proton.me>
2026-07-03 20:45:34 +0530

    Fix the message reply section

--- Frontend/src/components/MessageArea.jsx
+++ Frontend/src/components/MessageArea.jsx
@@ -792,9 +792,9 @@ const MessageArea = () => {
         "
                       >
 
-                        <p className="text-xs text-gray-500">
+                        <p className="text-xs text-orange-500 font-semibold mb-1">
 
-                          Replying to
+                          {msg.replyTo.sender?.toString() === userData?._id?.toString() ? "You" : (selectedUser?.name || selectedUser?.userName || "Someone")}
 
                         </p>
 
@@ -1219,7 +1219,7 @@ ${msg.sender?.toString() ===
                     "
                     >
 
-                      Replying
+                      {replyMessage.sender?.toString() === userData?._id?.toString() ? "You" : (selectedUser?.name || selectedUser?.userName || "Someone")}
 
                     </p>
 
