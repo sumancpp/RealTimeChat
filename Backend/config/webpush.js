542ffd012096ad2bea9e9413add9d24aacba17d2
Suman Maity <someone.cpp@proton.me>
2026-07-05 18:00:06 +0530

    add webPush

--- Backend/config/webpush.js
+++ Backend/config/webpush.js
@@ -2,10 +2,14 @@ import webpush from "web-push";
 import dotenv from "dotenv";
 dotenv.config();
 
-webpush.setVapidDetails(
-    'mailto:sumancoder404@gmail.com',
-    process.env.VAPID_PUBLIC_KEY,
-    process.env.VAPID_PRIVATE_KEY
-);
+if (process.env.VAPID_PUBLIC_KEY && process.env.VAPID_PRIVATE_KEY) {
+    webpush.setVapidDetails(
+        'mailto:sumancoder404@gmail.com',
+        process.env.VAPID_PUBLIC_KEY,
+        process.env.VAPID_PRIVATE_KEY
+    );
+} else {
+    console.warn("WARNING: VAPID keys are missing in environment variables. Web Push Notifications will be disabled.");
+}
 
 export default webpush;
