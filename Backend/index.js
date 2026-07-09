b218356c162427e4cf06f36e1944f01951fa0bfb
Suman Maity <someone.cpp@proton.me>
2026-07-09 19:07:01 +0530

    update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -90,7 +90,7 @@ const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
-    app.get("*", (req, res) => {
+    app.get(/.*/, (req, res) => {
         res.sendFile(path.join(__dirname, "../Frontend/dist", "index.html"));
     });
 }
