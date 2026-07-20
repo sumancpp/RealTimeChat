b7cefb950d70a968668401e8cc27359c26cf3403
Suman Maity <someone.cpp@proton.me>
2026-07-21 01:50:29 +0530

    fix again

--- Frontend/src/components/WatchTogether.jsx
+++ Frontend/src/components/WatchTogether.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import ReactPlayer from 'react-player/youtube';
+import ReactPlayer from 'react-player';
 import { getSocket } from '../socket';
 import { X, Play, Pause, Maximize2 } from 'lucide-react';
 
